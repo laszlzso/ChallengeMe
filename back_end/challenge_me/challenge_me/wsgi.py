cb443eb9f6805f251d3d7b9656429107a3611f81
Zsolt Laszlo <zotyarex@gmail.com>
2022-06-18 16:32:21 +0200

    Django init

---
+++ back_end/challenge_me/challenge_me/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for challenge_me project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'challenge_me.settings')
+
+application = get_wsgi_application()
