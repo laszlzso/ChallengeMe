6300933aca2c7b59777dc2d13cc87480a9936fc9
Zsolt Laszlo <zotyarex@gmail.com>
2022-06-19 17:52:22 +0200

    Back-end user authentication added

---
+++ back_end/challenge_me/user_auth/serializers.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        token['email'] = user.email
+        return token
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    # TODO(laszlzso): validate username uniqueness
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password2')
+
+    def validate(self, attrs):
+        if not attrs['username']:
+            raise serializers.ValidationError(
+                {'username': 'Invalid username'})
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {'password': 'Password fields didn\'t match'})
+
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
