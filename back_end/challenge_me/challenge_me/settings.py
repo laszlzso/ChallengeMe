d82e698d3836742a100706f50c3e9046ae55f0f4
richard.bata <richard.bata@transferwise.com>
2022-07-10 18:16:14 +0200

    ha

--- back_end/challenge_me/challenge_me/settings.py
+++ back_end/challenge_me/challenge_me/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&t9z$&-2%r*m1+)mvsokhiiw4yc)_2tjwxep&)_r27143j3qy@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('challenge_me_debug')
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
